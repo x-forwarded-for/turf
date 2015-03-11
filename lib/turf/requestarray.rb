5c882c1bdfb8002339c3f246b20a1cafbc77d55a
Thiébaud Weksteen <thiebaud@weksteen.fr>
2015-03-11 19:07:08 +1100

    Add inspect on RequestArray

--- lib/turf/requestarray.rb
+++ lib/turf/requestarray.rb
@@ -1,2 +1,17 @@
 class Turf::RequestArray < Array
+
+  def inspect
+    status = Hash.new 0
+    each { |r|
+      if r.response
+        status[r.response.status] += 1
+      else
+        status["unkown"] += 1
+      end
+    }
+    st = status.to_a.collect{ |k,v| "#{k}:#{v}" }.join(" ")
+    hostnames = collect { |r| r.hostname }.uniq.join(", ")
+    return "<#{length} ~ #{st} | #{hostnames}>"
+  end
+
 end
