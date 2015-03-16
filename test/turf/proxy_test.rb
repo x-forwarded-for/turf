0cb3f92a163c72f4a8a399b40a071a4343f139fb
Thiébaud Weksteen <thiebaud@weksteen.fr>
2015-03-17 10:07:39 +1100

    Add test for HTTPS requests through the proxy

--- test/turf/proxy_test.rb
+++ test/turf/proxy_test.rb
@@ -5,20 +5,39 @@ class ProxyTest < MiniTest::Test
   include DummyServer
 
   def start_forward_proxy
-    Thread.new {
-      rs = Turf::proxy :rules => [[ proc {|x| true}, :forward ]]
+    port = rand(1024..65535)
+    p = Thread.new {
+      rs = Turf::proxy :port => port, :rules => [
+        [ proc {|x| x.method == "CONNECT"}, :mitm_ssl],
+        [ proc {|x| true}, :forward ]
+      ]
     }
+    return p, port
   end
 
   def test_new
-    p = start_forward_proxy
+    p, p_port = start_forward_proxy
     ws, ws_port = start_basic_webrick
     wait_until_online '127.0.0.1', ws_port
-    wait_until_online '127.0.0.1', 8080
+    wait_until_online '127.0.0.1', p_port
 
     r = Turf::Request.new("GET / HTTP/1.1\r\n\r\n", hostname: "127.0.0.1",
                           port: ws_port, use_ssl: false)
-    r.run proxy: "http://127.0.0.1:8080"
+    r.run proxy: "http://127.0.0.1:#{p_port}"
+
+    p.raise IRB::Abort
+    ws.terminate
+  end
+
+  def test_mitm_ssl
+    p, p_port = start_forward_proxy
+    ws, ws_port = start_tls_webrick
+    wait_until_online '127.0.0.1', ws_port
+    wait_until_online '127.0.0.1', p_port
+
+    r = Turf::Request.new("GET / HTTP/1.1\r\n\r\n", hostname: "127.0.0.1",
+                          port: ws_port, use_ssl: true)
+    r.run proxy: "http://127.0.0.1:#{p_port}"
 
     p.raise IRB::Abort
     ws.terminate
