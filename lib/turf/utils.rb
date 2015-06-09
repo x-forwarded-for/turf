4fda1c3010c1346402bee9bf7bc9924f8d2f58d1
Thiébaud Weksteen <thiebaud@weksteen.fr>
2015-06-09 16:49:56 +1000

    Add TablePrinter

--- lib/turf/utils.rb
+++ lib/turf/utils.rb
@@ -10,5 +10,137 @@ module Turf
     "#{size > 9 || size.modulo(1) < 0.1 ? '%d' : '%.1f'}%s" % [size, unit]
   end
 
+  class TablePrinter
+
+    def initialize(requests, columns, width: nil)
+      @requests = requests
+      @width = width || 80
+      @columns = expand_columns(columns)
+      # Build all the rows with their contents,
+      # expanded_columns may be modified
+      @rows = build
+      # Find which columns overflow and adjust
+      adjust_width
+    end
+
+    def truncate(s, max_len)
+      s[0..max_len-1]
+    end
+
+    def character_rtruncate(s, max_len, c)
+      character_truncate(s.reverse, max_len, c).reverse
+    end
+
+    def character_truncate(s, max_len, c)
+      # Truncate the string, using a splitting character
+      if s.length > max_len
+        if s[1..-1].include? c
+          init_length = s.length
+          pl = init_length + 1
+          while s.length > max_len - 2
+            break if pl == s.length
+            pl = s.length
+            s = s.split(c, 2).last
+          end
+          if s.length != init_length
+            s = "\u2026" + c + s
+          end
+        end
+        if s.length > max_len
+          s = s[0..max_len] + "\u2026"
+        end
+      end
+      s
+    end
+
+    def expand_columns(columns)
+      # Each column must have the keys :name, :width, :cb, :weight.
+      # :adjust_cb must be defined if :weight != nil
+      columns.collect { |c|
+        if c.is_a?(Symbol)
+          column = Hash.new
+          column[:name] = c.to_s.capitalize
+          column[:width] = column[:name].length
+          column[:cb] = Proc.new { |x| x.send(c) }
+          column[:weight] = nil
+          column
+        else
+          if not c.has_key?(:name) or not c.has_key?(:cb)
+            raise Exception.new ":name and :cb are mandatory"
+          end
+          c[:width] ||= c[:name].length
+          c[:weight] ||= nil
+          c[:adjust_cb] ||= :truncate
+          c
+        end
+      }
+    end
+
+    def build
+      # Populate rows and update columns length if necessary
+      rows = @requests.collect do |r|
+          @columns.collect { |column|
+            v = column[:cb].call(r).to_s
+            n = column[:name]
+            if v.length > column[:width]
+              column[:width] = v.length
+            end
+            v
+        }
+      end
+    end
+
+    def render
+      thead = @columns.collect { |c| c[:name].ljust(c[:width]) }.join(" ")
+      tbody = @rows.collect { |row|
+                row.collect.with_index { |cell, i|
+                  col = @columns[i]
+                  if cell.length > col[:width]
+                    cell = send(col[:adjust_cb], cell, col[:width], col[:adjust_args])
+                  end
+                  cell.ljust(col[:width])
+                }.join(" ")
+              }.join("\n")
+      [thead, tbody].join("\n")
+    end
+
+    def adjust_width
+      adjustables = @columns.select { |c| c[:weight] }
+      fixed = @columns - adjustables
+      # Share the remaining width between all adjustables columns
+      loop do
+        done = true
+        remaining_width = @width - fixed.inject(0) {|m, c| m + c[:width] + 1}
+        total_weight = adjustables.inject(0) {|m, c| m + c[:weight]}
+        adjustables.each { |a|
+          allocated_width = remaining_width * (a[:weight].to_f/total_weight) - 1
+          # If we have over-allocated space for one column,
+          # redistribute the excess to the others
+          if allocated_width > a[:width]
+            adjustables.delete(a)
+            fixed << a
+            done = false
+          end
+        }
+        break if done
+      end
+      # For the columns that have not been succesfully adjusted
+      # truncate the width according to their preferences
+      remaining_width = @width - fixed.inject(0) {|m, c| m + c[:width] + 1}
+      total_weight = adjustables.inject(0) {|m, c| m + c[:weight]}
+      adjustables.each { |a|
+        a[:width] = remaining_width * (a[:weight].to_f/total_weight) - 1
+      }
+    end
+
+  end
+
+  module_function
+
+  def tp(requests, fields, width: nil)
+    tp = TablePrinter.new requests, fields, width: width
+    tp.render
+  end
+
 end
 
---
+++ test/turf/utils_test.rb
@@ -0,0 +1,28 @@
+require_relative '../test_helper'
+
+class UtilsTest < MiniTest::Test
+
+  def test_tableprinter
+    ra = Turf::get("http://this.is.a.very.long.domain.name.org/and/a/very/long/path/that/will/overflow/the/terminal/width/for/sure.html") * 10
+    fields = [{ name: "Hostname",
+                cb: Proc.new { |x| x.hostname},
+                weight: 0.2,
+                adjust_cb: :character_truncate,
+                adjust_args: "."
+             },
+              :port,
+             { name: "Path",
+                cb: Proc.new { |x| x.path },
+                weight: 0.2,
+                adjust_cb: :character_rtruncate,
+                adjust_args: "/"
+             },
+             { name: "Status",
+                cb: Proc.new { |x| x.response.nil? ? "-" : x.response.status}
+             }
+             ]
+    tp = Turf::TablePrinter.new(ra, fields, width: 80)
+    puts tp.render
+  end
+
+end
