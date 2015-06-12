4747d80439a0b3dc86e8fd916b780c6d5bb1aba0
Thiébaud Weksteen <thiebaud@weksteen.fr>
2015-06-12 15:56:17 +1000

    Warn if port already in use

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,30 +12,34 @@ def start_basic_webrick
   c = ConditionVariable.new
   port = rand(1025..65535)
 
-  ws = Thread.new {
-    abort_on_exception = true
-    server = WEBrick::HTTPServer.new(
-      Port: port,
-      AccessLog: [],
-      Logger: WEBrick::Log::new("/dev/null", 7),
-      BindAddress: '127.0.0.1',
-      StartCallback: Proc.new do
-        m.synchronize { c.signal }
-      end
-    )
-    server.mount_proc '/' do |req, res|
-      res["Content-Type"] = "text/plain"
-      res.body = 'Hello, world!'
-      if req.body and req.body.include? "def12"
-        res.status = "500"
-      end
-    end
-    server.start
-  }
   m.synchronize {
+    ws = Thread.new {
+      begin
+        server = WEBrick::HTTPServer.new(
+          Port: port,
+          AccessLog: [],
+          Logger: WEBrick::Log::new("/dev/stderr", 1),
+          BindAddress: '127.0.0.1',
+          StartCallback: Proc.new do
+            m.synchronize { c.signal }
+          end
+        )
+        server.mount_proc '/' do |req, res|
+          res["Content-Type"] = "text/plain"
+          res.body = 'Hello, world!'
+          if req.body and req.body.include? "def12"
+            res.status = "500"
+          end
+        end
+        server.start
+      rescue Exception => e
+        puts e.inspect
+        puts e
+      end
+    }
     c.wait(m)
+    return ws, port
   }
-  return ws, port
 end
 
 def start_tls_webrick
@@ -43,26 +47,35 @@ def start_tls_webrick
   c = ConditionVariable.new
   port = rand(1025..65535)
 
-  ws = Thread.new {
-    cert_name = [ %w[CN localhost], ]
-    server = WEBrick::HTTPServer.new(
-      Port: port,
-      AccessLog: [],
-      Logger: WEBrick::Log::new("/dev/null", 7),
-      SSLEnable: true,
-      SSLCertName: cert_name,
-      BindAddress: '127.0.0.1',
-      StartCallback: Proc.new do
-        m.synchronize { c.signal }
-      end
-    )
-    server.mount_proc '/' do |req, res|
-      res.body = 'Hello, world!'
-    end
-    server.start
-  }
   m.synchronize {
+    ws = Thread.new {
+      begin
+        cert_name = [ %w[CN localhost], ]
+        server = WEBrick::HTTPServer.new(
+          Port: port,
+          AccessLog: [],
+          Logger: WEBrick::Log::new("/dev/stderr", 1),
+          SSLEnable: true,
+          SSLCertName: cert_name,
+          BindAddress: '127.0.0.1',
+          StartCallback: Proc.new do
+            begin
+              m.synchronize { c.signal }
+            rescue Exception => e
+                puts e
+            end
+          end
+        )
+        server.mount_proc '/' do |req, res|
+          res.body = 'Hello, world!'
+        end
+        server.start
+      rescue Exception => e
+        puts e.inspect
+        puts e
+      end
+    }
     c.wait(m)
+    return ws, port
   }
-  return ws, port
 end
