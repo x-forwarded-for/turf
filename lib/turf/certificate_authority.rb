52b96c21125a7ff7fc003532bb927cb27e43e443
Luke Jahnke <luke.jahnke@gmail.com>
2015-08-05 01:51:38 +1000

    Clean up of CertificateAuthority

--- lib/turf/certificate_authority.rb
+++ lib/turf/certificate_authority.rb
@@ -1,102 +1,101 @@
 require 'openssl'
+require 'securerandom'
 
 module Turf
 
   class CertificateAuthority
 
-    def initialize
-      @key_path = File.join Turf.conf.path, "key.pem"
-      @cert_path = File.join Turf.conf.path, "cert.pem"
+    def certificate(hostname)
+      unless File.exist? certificate_path(hostname)
+        generate_certificate(hostname)
+      end
+      return OpenSSL::X509::Certificate.new(File.read(certificate_path(hostname)))
     end
 
     def key
-      if File.exist?(@key_path)
-        key = OpenSSL::PKey::RSA.new File.read @key_path
-      else
-        key = generate_ca_key
+      key_path = File.join(Turf.conf.path, "key.pem")
+      unless File.exist? key_path
+        generate_ca_key(key_path)
       end
+      return OpenSSL::PKey::RSA.new(File.read(key_path))
     end
 
     def ca_certificate
-      if File.exist?(@cert_path)
-        return OpenSSL::X509::Certificate.new File.read @cert_path
-      else
-        return generate_ca_cert
+      ca_cert_path = File.join(Turf.conf.path, "cert.pem")
+      unless File.exist?(ca_cert_path)
+        generate_ca_certificate(ca_cert_path)
       end
+      return OpenSSL::X509::Certificate.new(File.read(ca_cert_path))
     end
-
-    def certificate(hostname)
-      cert_path = File.join(Turf.conf.cert_dir, "#{hostname}.pem")
-      if File.exist?(cert_path)
-        return OpenSSL::X509::Certificate.new File.read cert_path
-      else
-        return generate_cert hostname
-      end
-    end
-
 
     private
 
+    def certificate_path(hostname)
+      # i think we are assuming cert_dir exists, could instead use FileUtils.mkdir_p
+      File.join(Turf.conf.cert_dir, "#{hostname}.pem")
+    end
+
     def generate_serial
-      Random.new.bytes(4).unpack("H*")[0].to_i(16)
+      SecureRandom.random_number(0xffffffff)
     end
 
-    def generate_ca_key
+    def generate_ca_key(key_path)
       key = OpenSSL::PKey::RSA.new 2048
-      open @key_path, 'w' do |io| io.write key.to_pem end
-      return key
+      File.open(key_path, 'w') do |f|
+        f.write(key.to_pem)
+      end
     end
 
-    def generate_ca_cert
+    def generate_ca_certificate(ca_cert_path)
       ca_name = OpenSSL::X509::Name.parse 'C=AU/O=X-Forwarded-For/CN=turf'
-      ca_cert = OpenSSL::X509::Certificate.new
-      ca_cert.serial = generate_serial
-      ca_cert.version = 2
-      ca_cert.not_before = Time.now
-      ca_cert.not_after = Time.now + (60 * 60 * 24 * 365 * 5)
+      ca_certificate = OpenSSL::X509::Certificate.new
+      ca_certificate.serial = generate_serial
+      ca_certificate.version = 2
+      ca_certificate.not_before = Time.now
+      ca_certificate.not_after = Time.now + (60 * 60 * 24 * 365 * 5)
 
-      ca_cert.public_key = key.public_key
-      ca_cert.subject = ca_name
-      ca_cert.issuer = ca_name
-
-      extension_factory = OpenSSL::X509::ExtensionFactory.new
-      extension_factory.subject_certificate = ca_cert
-      extension_factory.issuer_certificate = ca_cert
-
-      ca_cert.add_extension \
-        extension_factory.create_extension('subjectKeyIdentifier', 'hash')
-      ca_cert.add_extension \
-        extension_factory.create_extension('basicConstraints', 'CA:TRUE', true)
-
-      ca_cert.sign key, OpenSSL::Digest::SHA1.new
-      open @cert_path, 'w' do |io| io.write ca_cert.to_pem end
-      return ca_cert
-    end
-
-    def generate_cert(hostname)
-      name = OpenSSL::X509::Name.parse "O=Turf/CN=#{hostname}"
-      cert = OpenSSL::X509::Certificate.new
-      cert.version = 2
-      cert.serial = generate_serial
-      cert.not_before = Time.now
-      cert.not_after = Time.now + (60 * 60 * 24 * 365)
-
-      cert.public_key = key.public_key
-      cert.subject = name
-      cert.issuer = ca_certificate.subject
+      ca_certificate.public_key = key.public_key
+      ca_certificate.subject = ca_name
+      ca_certificate.issuer = ca_name
 
       extension_factory = OpenSSL::X509::ExtensionFactory.new
-      extension_factory.subject_certificate = cert
+      extension_factory.subject_certificate = ca_certificate
       extension_factory.issuer_certificate = ca_certificate
 
-      cert.sign key, OpenSSL::Digest::SHA1.new
-      open File.join(Turf.conf.cert_dir,
-          "#{hostname}.pem"), 'w' do |io|
-        io.write cert.to_pem
+      ca_certificate.add_extension(
+        extension_factory.create_extension('subjectKeyIdentifier', 'hash')
+      )
+      ca_certificate.add_extension(
+        extension_factory.create_extension('basicConstraints', 'CA:TRUE', true)
+      )
+
+      ca_certificate.sign key, OpenSSL::Digest::SHA1.new
+      File.open(ca_cert_path, 'w') do |f|
+        f.write(ca_certificate.to_pem)
       end
-      return cert
+    end
+
+    def generate_certificate(hostname)
+      name = OpenSSL::X509::Name.parse("O=Turf/CN=#{hostname}")
+      certificate = OpenSSL::X509::Certificate.new
+      certificate.version = 2
+      certificate.serial = generate_serial
+      certificate.not_before = Time.now
+      certificate.not_after = Time.now + (60 * 60 * 24 * 365)
+
+      certificate.public_key = key.public_key
+      certificate.subject = name
+      certificate.issuer = ca_certificate.subject
+
+      extension_factory = OpenSSL::X509::ExtensionFactory.new
+      extension_factory.subject_certificate = certificate
+      extension_factory.issuer_certificate = ca_certificate
+
+      certificate.sign(key, OpenSSL::Digest::SHA1.new)
+      File.open(certificate_path(hostname), 'w') do |f|
+        f.write(certificate.to_pem)
+      end
     end
 
   end
-
 end
