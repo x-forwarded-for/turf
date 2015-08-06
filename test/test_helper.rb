c4cce29163827fe63502ef6173289719abcb84c2
Luke Jahnke <luke.jahnke@gmail.com>
2015-08-07 02:36:00 +1000

    Untangled webrick test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,74 +13,80 @@ Turf.set_conf_path temp_dir
 Minitest.after_run { FileUtils.remove_entry_secure temp_dir }
 
 def start_basic_webrick
-  m = Mutex.new
-  c = ConditionVariable.new
+  mutex = Mutex.new
+  condition = ConditionVariable.new
   port = rand(1025..65535)
 
-  m.synchronize {
-    ws = Thread.new {
-      begin
-        server = WEBrick::HTTPServer.new(
-          Port: port,
-          AccessLog: [],
-          Logger: WEBrick::Log::new("/dev/stderr", 1),
-          BindAddress: '127.0.0.1',
-          StartCallback: Proc.new do
-            m.synchronize { c.signal }
-          end
-        )
-        server.mount_proc '/' do |req, res|
-          res["Content-Type"] = "text/plain"
-          res.body = 'Hello, world!'
-          if req.body and req.body.include? "def12"
-            res.status = "500"
-          end
-        end
-        server.start
-      rescue Exception => e
-        puts e.inspect
-        puts e
-      end
-    }
-    c.wait(m)
-    return ws, port
-  }
+  server = WEBrick::HTTPServer.new(
+    Port: port,
+    AccessLog: [],
+    Logger: WEBrick::Log::new("/dev/stderr", 1),
+    BindAddress: '127.0.0.1',
+    StartCallback: Proc.new do
+      mutex.synchronize { condition.signal }
+    end
+  )
+  server.mount_proc '/' do |req, res|
+    res["Content-Type"] = "text/plain"
+    res.body = 'Hello, world!'
+    if req.body and req.body.include? "def12"
+      res.status = "500"
+    end
+  end
+
+  webrick_thread = Thread.new do
+    begin
+      server.start
+    rescue Exception => e
+      puts e.inspect
+      puts e
+    end
+  end
+
+  mutex.synchronize { condition.wait(mutex) }
+  return webrick_thread, port
 end
 
 def start_tls_webrick
-  m = Mutex.new
-  c = ConditionVariable.new
+  mutex = Mutex.new
+  condition = ConditionVariable.new
   port = rand(1025..65535)
 
-  m.synchronize {
-    ws = Thread.new {
-      begin
-        cert_name = [ %w[CN localhost], ]
-        # unfortunately needed because webrick's ssl.rb has $stderr.putc
-        orig_stderr = $stderr
-        $stderr = StringIO.new
-        server = WEBrick::HTTPServer.new(
-          Port: port,
-          AccessLog: [],
-          Logger: WEBrick::Log::new("/dev/stderr", 1),
-          SSLEnable: true,
-          SSLCertName: cert_name,
-          BindAddress: '127.0.0.1',
-          StartCallback: Proc.new do
-            m.synchronize { c.signal }
-          end
-        )
-        $stderr = orig_stderr
-        server.mount_proc '/' do |req, res|
-          res.body = 'Hello, world!'
-        end
-        server.start
-      rescue Exception => e
-        puts e.inspect
-        puts e
+  cert_name = [ %w[CN localhost], ]
+
+  # unfortunately needed because webrick's ssl.rb has $stderr.putc
+  orig_stderr = $stderr
+  $stderr = StringIO.new
+
+  begin
+    server = WEBrick::HTTPServer.new(
+      Port: port,
+      AccessLog: [],
+      Logger: WEBrick::Log::new("/dev/stderr", 1),
+      SSLEnable: true,
+      SSLCertName: cert_name,
+      BindAddress: '127.0.0.1',
+      StartCallback: Proc.new do
+        mutex.synchronize { condition.signal }
       end
-    }
-    c.wait(m)
-    return ws, port
-  }
+    )
+  ensure
+    $stderr = orig_stderr
+  end
+
+  server.mount_proc '/' do |req, res|
+    res.body = 'Hello, world!'
+  end
+
+  webrick_thread = Thread.new do
+    begin
+      server.start
+    rescue Exception => e
+      puts e.inspect
+      puts e
+    end
+  end
+
+  mutex.synchronize { condition.wait(mutex) }
+  return webrick_thread, port
 end
