e8dec65244010ee7226a3b0b3ad359081116df0d
Thiébaud Weksteen <thiebaud@weksteen.fr>
2015-08-23 21:41:33 +1000

    Fixed Style/AlignArray

--- test/turf/utils_test.rb
+++ test/turf/utils_test.rb
@@ -4,22 +4,22 @@ class UtilsTest < MiniTest::Test
 
   def test_tableprinter
     ra = Turf.get("http://this.is.a.very.long.domain.name.org/and/a/very/long/path/that/will/overflow/the/terminal/width/for/sure.html") * 4
-    fields = [{ name: "Hostname",
+    fields = [ { name: "Hostname",
                 cb: Proc.new { |x| x.hostname },
                 weight: 0.2,
                 adjust_cb: :character_truncate,
                 adjust_args: "."
-             },
-              :port,
-             { name: "Path",
+               },
+               :port,
+               { name: "Path",
                 cb: Proc.new { |x| x.path },
                 weight: 0.2,
                 adjust_cb: :character_rtruncate,
                 adjust_args: "/"
-             },
-             { name: "Status",
-                cb: Proc.new { |x| x.response.nil? ? "-" : x.response.status }
-             }
+               },
+               { name: "Status",
+                  cb: Proc.new { |x| x.response.nil? ? "-" : x.response.status }
+               }
              ]
     tp = Turf::TablePrinter.new(ra, fields, width: 80)
     tos = tp.render
