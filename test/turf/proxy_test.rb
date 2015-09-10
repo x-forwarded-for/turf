2f6bdb693a964b240abb3fe64a5ce9ef866d3e03
Luke Jahnke <luke.jahnke@gmail.com>
2015-09-11 04:23:45 +1000

    Cleanup of proxy tests, mainly start_proxy

--- test/turf/proxy_test.rb
+++ test/turf/proxy_test.rb
@@ -4,19 +4,18 @@ require "net/http"
 
 class ProxyTest < MiniTest::Test
 
-  PROXY_RUNNING = /\ARunning on (.*):(?<proxy_port>[0-9]+)\z/
-
   class DummyUI
 
-    attr_accessor :proxy_thread
-    attr_accessor :message
     attr_reader :errors
+    attr_reader :infos
 
     def initialize
       @errors = []
+      @infos = []
     end
 
-    def info(s, from: nil)
+    def info(message, from: nil)
+      @infos << message
     end
 
     def ask(q)
@@ -31,36 +30,40 @@ class ProxyTest < MiniTest::Test
 
   def setup
     @ui = DummyUI.new
-    @m = Mutex.new
-    @started = ConditionVariable.new
   end
 
   def start_proxy
-    @m.synchronize {
-      t = Thread.new do
-        @m.synchronize {
-          @p = Turf::Proxy.new(port: 0, ui: @ui)
-          @p.bind
-          @started.signal
-        }
-        @p.serve
+    proxy = Turf::Proxy.new(port: 0, ui: @ui)
+    mutex = Mutex.new
+    proxy_started = ConditionVariable.new
+    proxy_thread = nil
+    mutex.synchronize do
+      proxy_thread = Thread.new do
+        Thread.current.abort_on_exception = true
+        mutex.synchronize do
+          proxy.bind
+          proxy_started.signal
+        end
+        proxy.serve
       end
-      @ui.proxy_thread = t
-      @started.wait(@m)
-      return t, @p.port
+      proxy_started.wait(mutex)
+    end
+    return { thread: proxy_thread,
+      proxy: proxy,
+      port: proxy.port,
     }
   end
 
   def test_irb_stop_after_start
     def @ui.info(message, from: nil)
-      @message = message
-      @proxy_thread.raise Interrupt
+      super
+      raise Interrupt
     end
 
-    @ui.proxy_thread, port = start_proxy
-    @ui.proxy_thread.join
-    assert_match(PROXY_RUNNING, @ui.message)
-    assert_empty(@p.requests)
+    p = start_proxy
+    p[:thread].join
+    assert_includes(@ui.infos, "Running on 127.0.0.1:#{p[:port]}")
+    assert_empty(p[:proxy].requests)
   end
 
   def test_one_request
@@ -68,29 +71,29 @@ class ProxyTest < MiniTest::Test
       "f"
     end
 
-    p, p_port = start_proxy
+    p = start_proxy
     ws, ws_port = start_basic_webrick
 
     r = Turf.get("http://127.0.0.1:#{ws_port}/")
-    r.run(proxy: "http://127.0.0.1:#{p_port}")
+    r.run(proxy: "http://127.0.0.1:#{p[:port]}")
 
-    p.raise Interrupt
-    p.join
+    p[:thread].raise Interrupt
+    p[:thread].join
     ws.terminate
-    assert_equal(1, @p.requests.length)
+    assert_equal(1, p[:proxy].requests.length)
   end
 
   def test_mitm_ssl
-    p, p_port = start_proxy
+    p = start_proxy
     ws, ws_port = start_tls_webrick
 
     r = Turf.get("https://127.0.0.1:#{ws_port}/")
-    r.run(proxy: "http://127.0.0.1:#{p_port}")
+    r.run(proxy: "http://127.0.0.1:#{p[:port]}")
 
-    p.raise Interrupt.new
-    p.join
+    p[:thread].raise Interrupt.new
+    p[:thread].join
     ws.terminate
-    assert_equal(2, @p.requests.length)
+    assert_equal(2, p[:proxy].requests.length)
   end
 
   def test_continue
@@ -98,25 +101,26 @@ class ProxyTest < MiniTest::Test
       "c"
     end
 
-    def @ui.info(s, from: nil)
+    def @ui.info(message, from: nil)
+      super
       # Need to raise second abort once the first one
       # has been processed.
-      if s.include?("Use Ctrl-C one more time to quit")
-        @proxy_thread.raise Interrupt
+      if message.include?("Use Ctrl-C one more time to quit")
+        raise Interrupt
       end
     end
 
-    p, p_port = start_proxy
+    p = start_proxy
     ws, ws_port = start_basic_webrick
 
     r = Turf.get("http://127.0.0.1:#{ws_port}/")
-    r.run(proxy: "http://127.0.0.1:#{p_port}")
+    r.run(proxy: "http://127.0.0.1:#{p[:port]}")
     assert_equal("200", r.response.status)
 
-    p.raise Interrupt
-    p.join
+    p[:thread].raise Interrupt
+    p[:thread].join
     ws.terminate
-    assert_equal(1, @p.requests.length)
+    assert_equal(1, p[:proxy].requests.length)
   end
 
   def test_drop
@@ -124,30 +128,30 @@ class ProxyTest < MiniTest::Test
       "d"
     end
 
-    p, p_port = start_proxy
+    p = start_proxy
     ws, ws_port = start_basic_webrick
 
     r = Turf::get("http://127.0.0.1:#{ws_port}/")
 
     assert_raises(EOFError) {
-      r.run(proxy: "http://127.0.0.1:#{p_port}")
+      r.run(proxy: "http://127.0.0.1:#{p[:port]}")
     }
 
-    p.raise Interrupt
-    p.join
+    p[:thread].raise Interrupt
+    p[:thread].join
     ws.terminate
 
-    assert_equal(1, @p.requests.length)
-    assert_nil(@p.requests[0].response)
+    assert_equal(1, p[:proxy].requests.length)
+    assert_nil(p[:proxy].requests[0].response)
   end
 
   def test_cert_rejected_followed_by_not_rejected
-    p, p_port = start_proxy
+    p = start_proxy
     ws, ws_port = start_tls_webrick
 
     uri = URI.parse("https://127.0.0.1:#{ws_port}/")
     req = Net::HTTP::Get.new(uri.request_uri)
-    con = Net::HTTP.new(uri.host, uri.port, "127.0.0.1", p_port)
+    con = Net::HTTP.new(uri.host, uri.port, "127.0.0.1", p[:port])
     con.use_ssl = true
 
     ex = assert_raises(OpenSSL::SSL::SSLError) do
@@ -158,13 +162,13 @@ class ProxyTest < MiniTest::Test
     con.verify_mode = OpenSSL::SSL::VERIFY_NONE
     res2 = con.request(req)
 
-    p.raise Interrupt.new
-    p.join
+    p[:thread].raise Interrupt.new
+    p[:thread].join
     ws.terminate
-    assert_equal(3, @p.requests.length)
-    assert_equal("CONNECT", @p.requests[0].method)
-    assert_equal("CONNECT", @p.requests[1].method)
-    assert_equal("GET", @p.requests[2].method)
+    assert_equal(3, p[:proxy].requests.length)
+    assert_equal("CONNECT", p[:proxy].requests[0].method)
+    assert_equal("CONNECT", p[:proxy].requests[1].method)
+    assert_equal("GET", p[:proxy].requests[2].method)
     assert_equal(2, @ui.errors.size)
     assert_includes(@ui.errors, "The certificate has been rejected by your client.")
     assert(@ui.errors.any? { |error| error.include? "unknown ca" })
